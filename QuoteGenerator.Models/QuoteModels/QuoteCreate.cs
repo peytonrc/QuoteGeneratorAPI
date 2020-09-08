b0fe1ec7e5456c4586d1fee48226facac3385104
peytonrc <peytnrose@gmail.com>
2020-09-08 12:32:35 -0400

    Added Quote CRUD

---
+++ QuoteGenerator.Models/QuoteModels/QuoteCreate.cs
@@ -0,0 +1,33 @@
+﻿using QuoteGenerator.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuoteGenerator.Models.QuoteModels
+{
+    public class QuoteCreate
+    {
+        [Required]
+        [MinLength(5, ErrorMessage = "This quote needs at least 5 characters")]
+        [MaxLength(500, ErrorMessage = "This quote cannot exceed 500 characters")]
+        [Display(Name = "Quote")]
+        public string Content { get; set; }
+
+        [Required]
+        [Display(Name = "Author")]
+        public int AuthorId { get; set; }
+      
+        [Required]
+        [Display (Name = "Category")]
+        public int CategoryId { get; set; }
+
+        [Display(Name = "Date Spoken YYYY/MM/DD")]
+        public DateTime DateSpoken { get; set; }
+
+        //public double Rating { get; set; } // Add foreign key
+    }
+}
---
+++ QuoteGenerator.Services/QuoteService.cs
@@ -0,0 +1,111 @@
+﻿using QuoteGenerator.Data;
+using QuoteGenerator.Models.QuoteModels;
+using QuoteGeneratorAPI.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuoteGenerator.Services
+{
+    public class QuoteService
+    {
+        private readonly Guid _userId;
+
+        public QuoteService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public IEnumerable<QuoteListItem> GetQuotes()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var quoteQuery =
+                    ctx
+                        .Quotes
+                        .Select(e => new QuoteListItem
+                        {
+                            QuoteId = e.QuoteId,
+                            Content = e.Content,
+                            AuthorId = e.AuthorId,
+                            AuthorName = e.Author.Name,
+                            CategoryId = e.CategoryId,
+                            CategoryName=e.Category.Name,
+                            DateSpoken= e.DateSpoken
+                        });
+
+                return quoteQuery.ToArray();
+            }
+        }
+        public bool CreateQuote(QuoteCreate model)
+        {
+            var entity = new Quote
+            {
+                Content = model.Content,
+                AuthorId = model.AuthorId,
+                CategoryId = model.CategoryId,
+                DateSpoken = model.DateSpoken
+            };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Quotes.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public QuoteDetail GetQuoteById(int quoteId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity = ctx.Quotes.SingleOrDefault(e => e.QuoteId == quoteId);
+
+                return
+                    new QuoteDetail
+                    {
+                        QuoteId = entity.QuoteId,
+                        Content = entity.Content,
+                        AuthorId = entity.AuthorId,
+                        AuthorName = entity.Author.Name,
+                        CategoryId = entity.CategoryId,
+                        CategoryName = entity.Category.Name,
+                        DateSpoken = entity.DateSpoken
+                    };
+            }
+        }
+
+
+        public bool UpdateQuote(QuoteEdit model)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Quotes
+                        .SingleOrDefault(e => e.QuoteId == model.QuoteId);
+
+                entity.Content = model.Content;
+                entity.DateSpoken = model.DateSpoken;
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public bool DeleteQuote(int quoteId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Quotes
+                        .SingleOrDefault(e => e.QuoteId == quoteId);
+
+                ctx.Quotes.Remove(entity);
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+    }
+}
